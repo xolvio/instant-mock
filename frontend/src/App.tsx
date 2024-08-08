bbd9995d43f06997c98011ef27744e81b97466bf
Michal Kawka <michal.kawka@gmx.com>
2024-08-09 00:20:24 +0200

    add header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,27 +3,22 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import ProposalDashboard from './components/ui/proposal-dashboard';
 import ProposalDetails from './components/ui/proposal-details';
 import SeedDetails from './components/ui/seed-details';
+import Header from "./components/ui/header";
 
 function App() {
-  return (
-    <Router>
-      {/*<header className="w-full bg-white shadow p-4">*/}
-      {/*    <div className="container mx-auto">*/}
-      {/*        <div className="text-2xl font-bold" style={{color: 'rgb(86, 191, 152)'}}>*/}
-      {/*            InstantMock by Xolvio*/}
-      {/*        </div>*/}
-      {/*    </div>*/}
-      {/*</header>*/}
-      <Routes>
-        <Route path="/" element={<ProposalDashboard />} />
-        <Route path="/proposals/:proposalId" element={<ProposalDetails />} />
-        <Route
-          path="/proposals/:proposalId/seeds/:seedId"
-          element={<SeedDetails />}
-        />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Header></Header>
+            <Routes>
+                <Route path="/" element={<ProposalDashboard/>}/>
+                <Route path="/proposals/:proposalId" element={<ProposalDetails/>}/>
+                <Route
+                    path="/proposals/:proposalId/seeds/:seedId"
+                    element={<SeedDetails/>}
+                />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/ui/header.tsx
@@ -0,0 +1,42 @@
+import logo from "../../assets/logo.png"
+import {CircleUser, Search} from "lucide-react";
+import {Input} from "./input";
+import {
+    DropdownMenu,
+    DropdownMenuContent, DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from "./dropdown-menu";
+import {Button} from "./button";
+import React from "react";
+
+const Header = () => {
+
+    return (
+        <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+            <img src={logo} alt="Logo" className="h-14 w-24 object-cover"/>
+            <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+                <form className="ml-auto flex-1 sm:flex-initial">
+                </form>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="secondary" size="icon" className="rounded-full">
+                            <CircleUser className="h-5 w-5"/>
+                            <span className="sr-only">Toggle user menu</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator/>
+                        <DropdownMenuItem>Settings</DropdownMenuItem>
+                        <DropdownMenuItem>Support</DropdownMenuItem>
+                        <DropdownMenuSeparator/>
+                        <DropdownMenuItem>Logout</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+        </header>)
+}
+
+export default Header;
