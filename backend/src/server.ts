c7b313e01373dc4af8d217efa5840492a7ecae9a
Combs <scombs14@ashvp142-053010.ppp.ford.com>
2024-08-20 14:06:01 -0400

    fix: support HTTP_PROXY env setting

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,57 +1,61 @@
-import express from "express";
-import path from "path";
-import cors from "cors";
-import mockInstances from "./mockInstances";
-import {initializeDatabase} from "./database/database";
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import mockInstances from './mockInstances';
+import {initializeDatabase} from './database/database';
 import seedsRoutes from './routes/seeds.routes';
 import proposalsRoutes from './routes/proposals.routes';
 import mocksRoutes from './routes/mocks.routes';
-import {MockService} from "./service/mockService";
-import {ProposalService} from "./service/proposalService";
-import proposals from "./proposals";
+import {MockService} from './service/mockService';
+import {ProposalService} from './service/proposalService';
+import proposals from './proposals';
 import swaggerJsdoc from 'swagger-jsdoc';
-import { bootstrap } from 'global-agent';
-import swaggerUi from "swagger-ui-express";
+import swaggerUi from 'swagger-ui-express';
+import * as Undici from 'undici';
 
+var proxy = require('express-http-proxy');
 
-// bootstrap();
-var proxy = require("express-http-proxy");
+require('dotenv').config();
 
-require("dotenv").config();
+const ProxyAgent = Undici.ProxyAgent;
+const setGlobalDispatcher = Undici.setGlobalDispatcher;
+console.log('process.env.HTTP_PROXY: ', process.env.HTTP_PROXY);
+if (process.env.HTTP_PROXY)
+  setGlobalDispatcher(new ProxyAgent(process.env.HTTP_PROXY));
 
 const app = express();
 const port = process.env.PORT || 3001;
-export const GRAPH_ID = "dev-federation-x02qb";
+export const GRAPH_ID = 'dev-federation-x02qb';
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../../frontend/build")));
-app.use("/api", seedsRoutes);
-app.use("/api", proposalsRoutes);
-app.use("/api", mocksRoutes);
+app.use(express.static(path.join(__dirname, '../../frontend/build')));
+app.use('/api', seedsRoutes);
+app.use('/api', proposalsRoutes);
+app.use('/api', mocksRoutes);
 const mockService = MockService.getInstance();
 const proposalService = new ProposalService();
 
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Instant Mock',
-            version: '1.0.0',
-        },
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Instant Mock',
+      version: '1.0.0',
     },
-    apis: [
-        path.join(__dirname, './routes/*.js'),
-        path.join(__dirname, 'server.js')
-    ],
+  },
+  apis: [
+    path.join(__dirname, './routes/*.js'),
+    path.join(__dirname, 'server.js'),
+  ],
 };
 
 const swaggerSpec = swaggerJsdoc(options);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 // Expose the OpenAPI JSON directly
 app.get('/api/openapi.json', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(swaggerSpec);
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
 });
 
 // Proxy setup to route requests dynamically based on proposal ID in the path
@@ -98,81 +102,83 @@ app.get('/api/openapi.json', (req, res) => {
  *                   example: "Error forwarding request"
  */
 app.use(
-    "/:proposalId/graphql", // Include proposalId as part of the route path
-    proxy(
-        (req: { params: { proposalId: string } }) => {
-            const proposalId = req.params.proposalId; // Extract proposalId from the path
+  '/:proposalId/graphql', // Include proposalId as part of the route path
+  proxy(
+    (req: {params: {proposalId: string}}) => {
+      const proposalId = req.params.proposalId; // Extract proposalId from the path
 
-            const proposal = proposals[proposalId];
-            const port = proposal.port;
-            mockService.startNewMockInstanceIfNeeded(proposalId).then(r => console.log(`Proxy to port ${port}`));
-            return `http://localhost:${port}`;
-        },
-        {
-            proxyReqPathResolver: () => {
-                return "/graphql";
-            },
-            userResDecorator: (proxyRes: any, proxyResData: any) => {
-                // Optionally modify the response before sending it back to the client
-                return proxyResData;
-            },
-        },
-    ),
+      const proposal = proposals[proposalId];
+      const port = proposal.port;
+      mockService
+        .startNewMockInstanceIfNeeded(proposalId)
+        .then((r) => console.log(`Proxy to port ${port}`));
+      return `http://localhost:${port}`;
+    },
+    {
+      proxyReqPathResolver: () => {
+        return '/graphql';
+      },
+      userResDecorator: (proxyRes: any, proxyResData: any) => {
+        // Optionally modify the response before sending it back to the client
+        return proxyResData;
+      },
+    }
+  )
 );
 
 // Optionally, create an endpoint to stop a mocking service
-app.post("/api/stop-mock", async (req, res) => {
-    const variantName = req.query.variantName as string;
+app.post('/api/stop-mock', async (req, res) => {
+  const variantName = req.query.variantName as string;
 
-    if (!variantName) {
-        return res.status(400).send("Proposal ID is required");
-    }
+  if (!variantName) {
+    return res.status(400).send('Proposal ID is required');
+  }
 
-    const instance = mockInstances[variantName];
-    if (!instance) {
-        return res
-            .status(404)
-            .send(`No active mocking service for proposal ID ${variantName}`);
-    }
+  const instance = mockInstances[variantName];
+  if (!instance) {
+    return res
+      .status(404)
+      .send(`No active mocking service for proposal ID ${variantName}`);
+  }
 
-    try {
-        await instance.service.stop();
-        delete mockInstances[variantName]; // Remove from the map
-        res.send({
-            message: `Mocking service for proposal ID ${variantName} stopped successfully`,
-        });
-    } catch (error) {
-        console.error("Error stopping mocking service:", error);
-        res.status(500).send("Error stopping mocking service");
-    }
+  try {
+    await instance.service.stop();
+    delete mockInstances[variantName]; // Remove from the map
+    res.send({
+      message: `Mocking service for proposal ID ${variantName} stopped successfully`,
+    });
+  } catch (error) {
+    console.error('Error stopping mocking service:', error);
+    res.status(500).send('Error stopping mocking service');
+  }
 });
 
-app.get("/api/server-status", (req, res) => {
-    const variantName = req.query.variantName as string;
+app.get('/api/server-status', (req, res) => {
+  const variantName = req.query.variantName as string;
 
-    if (!variantName) {
-        return res.status(400).send("Variant name is required");
-    }
+  if (!variantName) {
+    return res.status(400).send('Variant name is required');
+  }
 
-    const status = variantName in mockInstances ? "running" : "stopped";
-    res.json({status: status});
+  const status = variantName in mockInstances ? 'running' : 'stopped';
+  res.json({status: status});
 });
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/build", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
 });
 
 initializeDatabase()
-    .then(() => {
-        app.listen(port, () => {
-            console.log(`Server running on port ${port}`);
-        });
-        return proposalService.fetchProposalsFromApollo();
-    })
-    .then((proposals) => {
-        console.log("Proposals fetched");
-    })
-    .catch((error) => {
-        console.error("Error starting InstantMock:", error);
-        process.exit(1);
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
     });
+    return proposalService.fetchProposalsFromApollo();
+  })
+  .then((proposals) => {
+    console.log(`${proposals.length} Proposals fetched`);
+  })
+  .catch((error) => {
+    console.error('Error starting InstantMock:', error);
+    process.exit(1);
+  });
