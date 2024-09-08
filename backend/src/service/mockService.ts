c5adbab36157b09ddf353ce089cee4af3a1cb126
Michal Kawka <michal.kawka@gmx.com>
2024-09-08 09:27:59 +0200

    modify mockService logic to use graphId

--- backend/src/service/mockService.ts
+++ backend/src/service/mockService.ts
@@ -1,11 +1,9 @@
-import Client from '../graphql/client';
-import {SeedRepository} from '../repositories/seedRepository';
-import mockInstances from '../mockInstances';
-import {Seed} from '../models/seed';
-import {Proposal} from '../models/proposal';
-import proposals from '../proposals';
 import {Mutex} from 'async-mutex';
+import Client from '../graphql/client';
+import mockInstances from '../mockInstances';
 import MockServer from '../MockServer';
+import {Seed} from '../models/seed';
+import {SeedRepository} from '../repositories/seedRepository';
 import {SeedType} from '../seed/SeedManager';
 
 export class MockService {
@@ -27,20 +25,26 @@ export class MockService {
     return MockService.instance;
   }
 
-  async startNewMockInstanceIfNeeded(proposalId: string): Promise<MockServer> {
+  async getOrStartNewMockServer(
+    graphId: string,
+    variantName: string
+  ): Promise<MockServer> {
     const release = await this.mutex.acquire();
-    let mockInstance = mockInstances[proposalId];
+    let mockInstance = mockInstances[graphId]?.[variantName];
     if (!mockInstance) {
-      mockInstance = await this.startNewMockInstance(proposalId);
+      mockInstance = await this.startNewMockServer(graphId, variantName);
     }
     release();
     return mockInstance;
   }
 
-  async startNewMockInstance(proposalId: string): Promise<MockServer> {
-    // Fetch the SDL document for the given variantName
-    const schema = await this.client.getSchema(proposalId);
+  async startNewMockServer(
+    graphId: string,
+    variantName: string
+  ): Promise<MockServer> {
+    const schema = await this.client.getSchema(graphId, variantName);
 
+    // TODO remove this faker config
     const fakerConfig = {
       fmcc__account__AccountNumber: {
         method: 'random.numeric',
@@ -52,18 +56,13 @@ export class MockService {
       },
     };
 
-    // Proceed to start the mocking service using the fetched SDL
-    const proposal: Proposal = proposals[proposalId];
     const mockServer = new MockServer(schema, {
       subgraph: false,
       fakerConfig,
     });
-    // await mockingService.start();
-    // await mockingService.registerSchema(schema, {fakerConfig});
 
-    // Load seeds from the database and register them
     const seeds: Seed[] =
-      await this.seedRepository.findAllByVariantName(proposalId);
+      await this.seedRepository.findAllByVariantName(variantName);
 
     for (const seed of seeds) {
       mockServer.seedManager.registerSeed(seed.sequenceId, SeedType.Operation, {
@@ -72,18 +71,8 @@ export class MockService {
         operationMatchArguments: JSON.parse(seed.operationMatchArguments),
       });
     }
-    // Store the instance information in the map
-    // const mockInstance = {port, service: mockingService} as MockInstance;
-    mockInstances[proposalId as string] = mockServer;
+    mockInstances[graphId] = mockInstances[graphId] || {};
+    mockInstances[graphId][variantName] = mockServer;
     return mockServer;
   }
-
-  // async restartMockInstance(proposalId: string): Promise<void> {
-  //   const mockServer: MockServer = mockInstances[proposalId];
-  //   if (mockServer) {
-  //     const service = mockServer.service.stop();
-  //     delete mockInstances[proposalId as string];
-  //     await this.startNewMockInstanceIfNeeded(proposalId);
-  //   }
-  // }
 }
