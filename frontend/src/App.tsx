c2585184d02f5f752ab202410ccb695ab17b94ef
Michal Kawka <michal.kawka@gmx.com>
2024-09-08 09:27:35 +0200

    add new route to the client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,32 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import ProposalDashboard from './components/ui/proposal-dashboard';
-import ProposalDetails from './components/ui/proposal-details';
+import GraphDashboard from './components/ui/graph-dashboard';
+import Header from './components/ui/header';
 import SeedDetails from './components/ui/seed-details';
-import Header from "./components/ui/header";
+import VariantDashboard from './components/ui/variant-dashboard';
+import VariantDetails from './components/ui/variant-details';
 
 function App() {
-    return (
-        <Router>
-            <Header></Header>
-            <Routes>
-                <Route path="/" element={<ProposalDashboard/>}/>
-                <Route path="/proposals/:proposalId" element={<ProposalDetails/>}/>
-                <Route
-                    path="/proposals/:proposalId/seeds/:seedId"
-                    element={<SeedDetails/>}
-                />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Header></Header>
+      <Routes>
+        <Route path="/graphs" element={<GraphDashboard />} />
+        <Route
+          path="/graphs/:graphId/variants"
+          element={<VariantDashboard />}
+        />
+        <Route
+          path="/graphs/:graphId/variants/:variantName"
+          element={<VariantDetails />}
+        />
+        <Route
+          path="/proposals/:proposalId/seeds/:seedId"
+          element={<SeedDetails />}
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
