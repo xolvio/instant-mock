e003383a1b7cef31b5682a24e767dbb8302d4e90
Michal Kawka <michal.kawka@gmx.com>
2024-09-08 09:26:59 +0200

    use graphId inside of the mockController.ts

--- backend/src/controllers/mockController.ts
+++ backend/src/controllers/mockController.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from "express";
-import mockInstances from "../mockInstances";
-import { MockService } from "../service/mockService";
+import {Request, Response} from 'express';
+import mockInstances from '../mockInstances';
+import {MockService} from '../service/mockService';
 
 export default class MockController {
   private mockService: MockService;
@@ -11,29 +11,34 @@ export default class MockController {
   }
 
   async startNewMockInstance(req: Request, res: Response) {
-    const { variantName } = req.query;
+    const {graphId, variantName} = req.query;
     if (!variantName) {
-      return res.status(400).send("Variant Name is required");
+      return res.status(400).send('Variant Name is required');
     }
 
-    if (mockInstances[variantName as string]) {
-      return res
-        .status(409)
-        .send(
-          `A mocking service is already running for variant name ${variantName} on port ${mockInstances[variantName as string]}`,
-        );
+    if (mockInstances[graphId as string][variantName as string]) {
+      return (
+        res
+          .status(409)
+          //   TODO remove that
+          .send(
+            `A mocking service is already running for variant name ${variantName} on port ${mockInstances[variantName as string]}`
+          )
+      );
     }
 
     try {
-      await this.mockService.startNewMockInstance(variantName as string);
+      await this.mockService.startNewMockServer(
+        graphId as string,
+        variantName as string
+      );
       res.json({
         variantName,
         message: `Mocking service started with SDL from ${variantName}`,
       });
     } catch (error) {
-      console.error("Error in starting mocking service:", error);
-      res.status(500).send("Error starting mocking service");
+      console.error('Error in starting mocking service:', error);
+      res.status(500).send('Error starting mocking service');
     }
   }
 }
-
