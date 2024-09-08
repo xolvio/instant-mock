9523f1d0717f5b9335b80722bc529517dde84262
Michal Kawka <michal.kawka@gmx.com>
2024-09-08 09:30:50 +0200

    add proposal-card

--- frontend/src/components/ui/proposal-card.tsx
+++ frontend/src/components/ui/proposal-card.tsx
@@ -1,32 +1,43 @@
-import React from "react";
-import {Card, CardDescription, CardHeader, CardTitle} from "./card";
-import {Badge} from "./badge";
-import {Proposal} from "../../models/Proposal"
-import {Clock3, User} from "lucide-react";
-import {Link} from "react-router-dom";
+import {Proposal} from '@/models/Graph';
+import {Clock3, User} from 'lucide-react';
+import React from 'react';
+import {Link, useParams} from 'react-router-dom'; // Import useParams
+import {Badge} from './badge';
+import {Card, CardDescription, CardHeader, CardTitle} from './card';
 
+const ProposalCard: React.FC<Proposal> = ({
+  id,
+  displayName,
+  createdBy,
+  createdAt,
+  status,
+  backingVariant,
+}) => {
+  const {graphId} = useParams<{graphId: string}>();
 
-const ProposalCard: React.FC<Proposal> = ({id, title, author, created, status}) => (
-    <Link to={`/proposals/${id}`}>
-        <Card className="mb-4">
-            <CardHeader>
-                <CardTitle>{title}</CardTitle>
-                <CardDescription>
-                    <div className="flex items-center mb-2">
-                        <User className="mr-1.5 h-4 w-4 text-gray-500"/> {/* Added icon */}
-                        <span>Created by {author}</span>
-                    </div>
-                    <div className="flex items-center mb-2">
-                        <Clock3 className="mr-1.5 h-4 w-4 text-gray-500"/> {/* Added icon */}
-                        <span>Created on {new Date(created).toLocaleDateString()}</span>
-                    </div>
-                    <Badge className="mt-2" variant="secondary">{status}</Badge>
-                </CardDescription>
-            </CardHeader>
-        </Card>
+  return (
+    <Link to={`/graphs/${graphId}/variants/${backingVariant.name}`}>
+      {' '}
+      <Card className="mb-4">
+        <CardHeader>
+          <CardTitle>{displayName}</CardTitle>
+          <CardDescription>
+            <div className="flex items-center mb-2">
+              <User className="mr-1.5 h-4 w-4 text-gray-500" />
+              <span>Created by {createdBy.name}</span>
+            </div>
+            <div className="flex items-center mb-2">
+              <Clock3 className="mr-1.5 h-4 w-4 text-gray-500" />
+              <span>Created on {new Date(createdAt).toLocaleDateString()}</span>
+            </div>
+            <Badge className="mt-2" variant="secondary">
+              {status}
+            </Badge>
+          </CardDescription>
+        </CardHeader>
+      </Card>
     </Link>
-);
+  );
+};
 
-export {
-    ProposalCard,
-}
+export {ProposalCard};
