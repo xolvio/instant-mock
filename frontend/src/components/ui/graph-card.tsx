a42fdf974186e06e37170b149c722bcdf2e2897c
Michal Kawka <michal.kawka@gmx.com>
2024-09-09 10:56:43 +0200

    add collapse to the graph-card.tsx

--- frontend/src/components/ui/graph-card.tsx
+++ frontend/src/components/ui/graph-card.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import {ChevronDown, ChevronUp} from 'lucide-react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import {
   Table,
@@ -9,38 +12,67 @@ import {
   TableRow,
 } from '../../components/ui/table';
 import {Graph} from '../../models/Graph';
+import {Button} from './button';
 import {Card, CardContent, CardHeader, CardTitle} from './card';
 import {Separator} from './separator';
 
 const GraphCard: React.FC<Graph> = (graph: Graph) => {
+  dayjs.extend(relativeTime);
+
+  const [isContentVisible, setIsContentVisible] = useState(true);
+
+  const toggleContentVisibility = () => {
+    setIsContentVisible(!isContentVisible);
+  };
+
   return (
     <Link to={`/graphs/${graph.id}/variants`}>
       <Card className="mb-4">
-        <CardHeader>
+        <CardHeader className="flex flex-row items-center justify-between">
           <CardTitle>{graph.name}</CardTitle>
+          {/*TODO maybe add tooltip?*/}
+          <Button
+            variant={'ghost'}
+            size={'icon'}
+            onClick={(e) => {
+              e.preventDefault(); // Prevent Link navigation when clicking the button
+              toggleContentVisibility();
+            }}
+          >
+            {isContentVisible ? (
+              <ChevronDown className="h-4 w-4 text-muted-foreground" />
+            ) : (
+              <ChevronUp className="h-4 w-4 text-muted-foreground" />
+            )}
+          </Button>
         </CardHeader>
-        <CardContent>
-          <Separator />
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Variant name</TableHead>
-                <TableHead>Last published</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {graph.variants.map((variant) => (
-                <TableRow key={variant.name}>
-                  <TableCell className="font-medium">{variant.name}</TableCell>
-                  <TableCell>
-                    {/*TODO format this date to use 2 hours ago, 3 monhts ago, last year, etc...*/}
-                    {variant.latestPublication?.publishedAt}
-                  </TableCell>
+        {isContentVisible && (
+          <CardContent>
+            <Separator />
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Variant name</TableHead>
+                  <TableHead>Last published</TableHead>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </CardContent>
+              </TableHeader>
+              <TableBody>
+                {graph.variants.map((variant) => (
+                  <TableRow key={variant.name}>
+                    <TableCell className="font-medium">
+                      {variant.name}
+                    </TableCell>
+                    <TableCell>
+                      {variant.latestPublication?.publishedAt
+                        ? dayjs(variant.latestPublication.publishedAt).fromNow()
+                        : 'never'}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </CardContent>
+        )}
       </Card>
     </Link>
   );
