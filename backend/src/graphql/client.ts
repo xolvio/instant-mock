45711d94d65d53ddfaaf695a7c4c8c09c944a020
Michal Kawka <michal.kawka@gmx.com>
2024-09-18 21:35:09 +0200

    remove instant mock link

--- backend/src/graphql/client.ts
+++ backend/src/graphql/client.ts
@@ -20,40 +20,6 @@ export default class Client {
       uri: 'https://api.apollographql.com/api/graphql',
     });
 
-    const instantMockLink = new ApolloLink((operation, forward) => {
-      return forward(operation).map((response) => {
-        const graphId = 'dev-federation-x02qb';
-        const variantName = 'p-98';
-        const operationName = operation.operationName;
-        const operationMatchArguments = operation.variables;
-        const seedResponse = response.data;
-
-        const apiUrl = `http://localhost:3001/api/seeds?variantName=${variantName}&graphId=${graphId}`;
-
-        fetch(apiUrl, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            seedResponse: seedResponse,
-            sequenceId: 'test',
-            operationName: operationName,
-            operationMatchArguments: operationMatchArguments,
-          }),
-        })
-          .then((res) => res.json())
-          .then((json) => {
-            console.log('Seed registered successfully:', json);
-          })
-          .catch((err) => {
-            console.error('Error creating seed:', err);
-          });
-
-        return response;
-      });
-    });
-
     const authLink = setContext((_, {headers}) => {
       return {
         headers: {
