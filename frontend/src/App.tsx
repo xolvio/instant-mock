fab3c75cfce9dab33144fb42db0ade7ec9e598ae
Michal Kawka <michal.kawka@gmx.com>
2024-09-24 21:46:38 +0200

    add home route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import GraphDashboard from './components/ui/graph-dashboard';
 import Header from './components/ui/header';
+import Home from './components/ui/home';
+import NotFound from './components/ui/not-found';
 import SeedDetails from './components/ui/seed-details';
+import SettingsPage from './components/ui/settings';
 import VariantDashboard from './components/ui/variant-dashboard';
 import VariantDetails from './components/ui/variant-details';
 
@@ -11,6 +14,7 @@ function App() {
     <Router>
       <Header></Header>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/graphs" element={<GraphDashboard />} />
         <Route
           path="/graphs/:graphId/variants"
@@ -24,6 +28,8 @@ function App() {
           path="/proposals/:proposalId/seeds/:seedId"
           element={<SeedDetails />}
         />
+        <Route path="settings" element={<SettingsPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
