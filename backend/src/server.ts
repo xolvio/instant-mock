7070a1cd2307ccc3c3540a29a71112761cfb1989
Michal Kawka <michal.kawka@gmx.com>
2024-09-24 21:51:09 +0200

    delete mock routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,6 @@ import swaggerUi from 'swagger-ui-express';
 import * as Undici from 'undici';
 import {initializeDatabase} from './database/database';
 import graphsRoutes from './routes/graphs.routes';
-import mocksRoutes from './routes/mocks.routes';
 import seedsRoutes from './routes/seeds.routes';
 import {SeededOperationResponse} from './seed/types';
 import {MockService} from './service/mockService';
@@ -35,7 +34,6 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../../frontend/build')));
 
 app.use('/api', seedsRoutes);
-app.use('/api', mocksRoutes);
 app.use('/api', graphsRoutes);
 app.get('*', (_, res) => {
   res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
