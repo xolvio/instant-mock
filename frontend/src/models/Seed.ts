ac8e324aec02c90b70d8e9fdf5f30f112d0e0407
Michal Kawka <michal.kawka@gmx.com>
2024-09-24 23:24:31 +0200

    update seed type

--- frontend/src/models/Seed.ts
+++ frontend/src/models/Seed.ts
@@ -1,8 +1,9 @@
 export type Seed = {
-    id: number;
-    operationName: string;
-    seedResponse: string;
-    operationMatchArguments: string;
-    sequenceId: string;
-    variantName: string;
-}
+  id: number;
+  operationName: string;
+  seedResponse: string;
+  operationMatchArguments: string;
+  sequenceId: string;
+  variantName: string;
+  graphId: string;
+};
