e71e8d13a01e82f922274033897addcbcf5dfba4
Michal Kawka <michal.kawka@gmx.com>
2024-11-07 16:04:23 +0100

    cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from "cypress";
+import {defineConfig} from "cypress";
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Example: setting up a custom plugin
+      on('before:run', () => {
+        console.log("Before running tests");
+      });
+
+      // Optionally, you could return the updated config
+      return config;
     },
+    specPattern: "cypress/e2e/**/*.cy.ts", // set up spec file pattern with TypeScript
+    baseUrl: "http://localhost:3000", // set your app's base URL
   },
 });
