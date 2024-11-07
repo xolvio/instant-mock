86904c2f7367700450d53c901d61fca56bd3cb37
Michal Kawka <michal.kawka@gmx.com>
2024-11-07 20:36:43 +0100

    increase payload size

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,29 +1,29 @@
 //Dont move these
 import 'reflect-metadata';
-require('dotenv').config();
-
 import {
   EntityManager,
   EntityRepository,
   MikroORM,
   RequestContext,
 } from '@mikro-orm/core';
-import {Seed} from './models/seed';
-import {SeedGroup} from './models/seedGroup';
 import cors from 'cors';
 import express from 'express';
-import * as Undici from 'undici';
 import path from 'path';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
+import * as Undici from 'undici';
+import Client from './graphql/client';
+import {ApolloApiKey} from './models/apolloApiKey';
+import {Seed} from './models/seed';
+import {SeedGroup} from './models/seedGroup';
+import apolloApiKeysRoutes from './routes/apolloApiKey';
 import graphqlRoutes from './routes/graphql';
 import graphsRoutes from './routes/graphs';
 import proposalsRoutes from './routes/proposals';
-import seedsRoutes from './routes/seeds';
 import seedGroupsRoutes from './routes/seedGroups';
-import apolloApiKeysRoutes from './routes/apolloApiKey';
-import Client from './graphql/client';
-import {ApolloApiKey} from './models/apolloApiKey';
+import seedsRoutes from './routes/seeds';
+
+require('dotenv').config();
 
 const isTypescript = __filename.endsWith('.ts');
 
@@ -81,6 +81,8 @@ const port = process.env.PORT || 3007;
     await em.persistAndFlush(newGroup);
   }
 
+  app.use(express.json({limit: '50mb'}));
+  app.use(express.urlencoded({limit: '50mb', extended: true}));
   app.use(cors());
   app.use(express.json());
   app.use((_, __, next) => RequestContext.create(DI.orm.em, next));
