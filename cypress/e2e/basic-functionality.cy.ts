b8426fc5f36bf0bbcc754f3739203e321cbbe086
Sam J Combs <samueljcombs@gmail.com>
2024-11-11 10:55:59 -0500

    e2e test path

--- cypress/e2e/basic-functionality.cy.ts
+++ cypress/e2e/basic-functionality.cy.ts
@@ -33,21 +33,122 @@ describe("InstantMock Basic Tests", () => {
         cy.task("log", `Iframe found with src: ${iframe.prop("src")}`);
       });
 
-    // Wait for iframe to be ready and check for textarea presence within iframe
+    cy.wait(4000);
+
     cy.iframe()
       .find("textarea", { timeout: 3000 })
-      .should("be.visible")
       .then((textareas) => {
         cy.task("log", `Found ${textareas.length} textareas in iframe.`);
+        expect(textareas.length).to.eq(
+          4,
+          "Expected 4 textareas to be found in the iframe.",
+        );
 
         // Log each textarea and attempt typing
         textareas.each((index, textarea) => {
-          cy.wrap(textarea)
-            .type(`Sample text ${index}`, { force: true })
-            .then(() =>
-              cy.task("log", `Typed into textarea #${index} in iframe.`),
-            );
+          if (index === 1)
+            cy.wrap(textarea)
+              .type(Cypress.platform === "darwin" ? "{cmd}a" : "{ctrl}a", {
+                force: true,
+              })
+              .type(
+                `query Product($productId: ID!) {
+product(id: $productId) {
+  name
+  package
+}`,
+                { force: true },
+              )
+              .then(() => cy.task("log", `Typed operation into textarea`));
+          if (index === 2)
+            cy.wrap(textarea)
+              .clear({ force: true })
+              .type(
+                `{
+  "productId": "1"`,
+                { force: true },
+              )
+              .then(() => cy.task("log", `Typed variables into textarea`));
         });
       });
+
+    //     // Wait for iframe to be ready and check for textarea presence within iframe
+    //     cy.iframe()
+    //       .should("be.visible")
+    //       .find("textarea", { timeout: 3000 })
+    //       .then((textareas) => {
+    //         cy.task("log", `Found ${textareas.length} textareas in iframe.`);
+    //
+    //         // Log each textarea and attempt typing
+    //         textareas.each((index, textarea) => {
+    //           if (index === 1)
+    //             cy.wrap(textarea)
+    //               .type(Cypress.platform === "darwin" ? "{cmd}a" : "{ctrl}a", {
+    //                 force: true,
+    //               })
+    //               .type(
+    //                 `query Product($productId: ID!) {
+    // product(id: $productId) {
+    //   name
+    //   package
+    // }`,
+    //                 { force: true },
+    //               )
+    //               .then(() => cy.task("log", `Typed operation into textarea`));
+    //           if (index === 2)
+    //             cy.wrap(textarea)
+    //               .clear({ force: true })
+    //               .type(
+    //                 `{
+    //   "productId": "1"`,
+    //                 { force: true },
+    //               )
+    //               .then(() => cy.task("log", `Typed variables into textarea`));
+    //         });
+    //       });
+
+    cy.wait(1000);
+
+    cy.iframe()
+      .find('[data-testid="run_operation_button"]')
+      .should("be.visible") // Ensure the button is visible
+      .click({ force: true })
+      .then(() => cy.task("log", `Clicked Run Operation button`));
+
+    cy.wait(1000);
+
+    cy.get("#create-seed-from-response")
+      .should("be.visible")
+      .click()
+      .then(() => cy.task("log", `Clicked Create Seed From Response button`));
+
+    cy.get('[name="seedResponse"]').clear().type(`{
+  "data": {
+    "product": {
+      "name": "Goodbye World",
+      "package": "Goodbye World Again!",
+      "__typename": "Product"
+    },
+    "__typename": "Query"
+  }
+}`);
+
+    cy.get("#save-seed-button")
+      .should("be.visible")
+      .click()
+      .then(() => cy.task("log", `Clicked Save Seed button`));
+
+    cy.get('button[role="tab"]')
+      .contains("SANDBOX")
+      .click()
+      .then(() => cy.task("log", "clicked sandbox tab button"));
+
+    cy.wait(6000);
+
+    cy.iframe()
+      .find('[data-testid="run_operation_button"]')
+      .should("be.visible") // Ensure the button is visible
+      .click({ force: true })
+      .then(() => cy.task("log", `Clicked Run Operation button again`));
   });
 });
