ab2ef98c397a25631c6a9bf0e4407bc16d8930aa
Mike Astle <astletron@gmail.com>
2025-03-05 13:44:26 -0500

    chore: support protocol env var in fe via static bundle endpoint (#50)

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,9 +1,10 @@
 export const config = {
   env: process.env.NODE_ENV as 'development' | 'production' | 'test',
   backend: {
-    url: typeof window !== 'undefined' && window.__RUNTIME_CONFIG__
-      ? `${window.__RUNTIME_CONFIG__.BACKEND_URL}${window.__RUNTIME_CONFIG__.BACKEND_PORT ? `:${window.__RUNTIME_CONFIG__.BACKEND_PORT}` : ''}`
-      : `${process.env.REACT_APP_BACKEND_URL || 'http://localhost'}:${process.env.REACT_APP_BACKEND_PORT || '3033'}`,
+    url:
+      typeof window !== 'undefined' && window.__RUNTIME_CONFIG__
+        ? `${window.__RUNTIME_CONFIG__.BACKEND_PROTO || 'http'}://${window.__RUNTIME_CONFIG__.BACKEND_URL}${window.__RUNTIME_CONFIG__.BACKEND_PORT ? `:${window.__RUNTIME_CONFIG__.BACKEND_PORT}` : ''}`
+        : `${process.env.REACT_APP_BACKEND_PROTO || 'http'}://${process.env.REACT_APP_BACKEND_URL || 'localhost'}:${process.env.REACT_APP_BACKEND_PORT || '3033'}`,
   },
   frontend: {
     url: `${process.env.REACT_APP_FRONTEND_URL || 'http://localhost'}:${process.env.REACT_APP_FRONTEND_PORT || '3032'}`,
